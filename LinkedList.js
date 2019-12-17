a103e6dede47bdea2a47320fae051ac8f610658c
Fanny <fn2939747187@gmail.com>
2019-12-18 00:07:26 +0800

    Simple Linkedlist

---
+++ LinkedList.js
@@ -0,0 +1,153 @@
+let LinkedList = function () {
+    this.head = null;
+    this.length = 0;
+    let node = function (data) {
+        this.data = data;
+        this.next = null;
+    };
+    LinkedList.prototype.append = function (data) {
+        let newNode = new node(data);
+        this.length++;
+        if (!this.head) {
+            this.head = newNode;
+            return;
+        }
+        let current = this.head;
+        while (current) {
+            if (current.next) {
+                current = current.next;
+            } else {
+                current.next = newNode;
+                return;
+            }
+        }
+    };
+    LinkedList.prototype.removeAt = function (position) {
+        if (this.isEmpty()) return false;
+        let count = 0, current = this.head;
+        if (!position) {
+            this.head = this.head.next;
+            this.length--;
+            return true;
+        }
+        while (current.next) {
+            if (position - 1 === count) {
+
+                if (current.next.next)
+                    current.next = current.next.next;
+                else
+                    current.next = null;
+                this.length--;
+                return true;
+            } else {
+                current = current.next;
+                count++;
+            }
+        }
+    };
+    LinkedList.prototype.remove = function (data) {
+        if (this.isEmpty()) return undefined;
+        let current = this.head, count = 0;
+        while (current.next) {
+            if (current.next.data === data) {
+                if (current.next.next)
+                    current.next = current.next.next;
+                else
+                    current.next = null;
+                this.length--;
+            } else {
+                current = current.next;
+                count++;
+            }
+        }
+
+    };
+    LinkedList.prototype.insert = function (data, position) {
+        if (this.isEmpty()) return undefined;
+        let current = this.head, count = 0, newNode = new node(data);
+        while (current) {
+            if (count === position) {
+                newNode.next = current.next;
+                current.next = newNode;
+                this.length++;
+                return true;
+            } else {
+                current = current.next;
+                count++;
+            }
+        }
+    };
+    LinkedList.prototype.get = function (position) {
+        if (this.isEmpty()) return undefined;
+        let current = this.head, count = 0;
+        while (current) {
+            if (count === position) {
+                return current.data;
+            } else {
+                current = current.next;
+                count++;
+            }
+        }
+
+    };
+    LinkedList.prototype.update = function (position, data) {
+        if (this.isEmpty()) return undefined;
+        let current = this.head, count = 0;
+        while (current) {
+            if (count === position) {
+                current.data = data;
+                return;
+            } else {
+                current = current.next;
+                count++;
+            }
+        }
+    };
+    LinkedList.prototype.indexOf = function (data) {
+        if (this.isEmpty()) return -1;
+        let current = this.head, count = 0;
+        while (current) {
+            if (current.data === data) return count;
+            else {
+                current = current.next;
+                count++;
+            }
+        }
+        return -1;
+
+    };
+    LinkedList.prototype.isEmpty = function () {
+        return !this.length;
+    };
+    LinkedList.prototype.size = function () {
+        return this.length;
+    }
+};
+
+//test
+
+let linkedlist = new LinkedList();
+linkedlist.append(1);
+linkedlist.append(2);
+linkedlist.append(3);
+linkedlist.append(4);
+linkedlist.append(5);
+console.log(linkedlist);
+
+linkedlist.removeAt(0);
+linkedlist.removeAt(1);
+console.log(linkedlist);
+
+linkedlist.remove(3);
+console.log(linkedlist);
+
+linkedlist.insert(10, 2);
+console.log(linkedlist);
+
+console.log(linkedlist.get(2));
+
+linkedlist.update(2,15);
+console.log(linkedlist);
+
+console.log(linkedlist.indexOf(15));
+console.log(linkedlist.size());
